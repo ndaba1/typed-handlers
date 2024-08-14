c1cc10d897e80d67eba90ed3cf16621c00203b73
Victor Ndaba <mwangindaba@gmail.com>
2024-08-14 20:17:03 +0300

    chore: add vercel analytics

--- docs/astro.config.mjs
+++ docs/astro.config.mjs
@@ -1,8 +1,15 @@
-import { defineConfig } from 'astro/config';
-
 import tailwind from "@astrojs/tailwind";
+import { defineConfig } from "astro/config";
+
+import vercel from "@astrojs/vercel/serverless";
 
 // https://astro.build/config
 export default defineConfig({
-  integrations: [tailwind()]
+  integrations: [tailwind()],
+  output: "server",
+  adapter: vercel({
+    webAnalytics: {
+      enabled: true,
+    },
+  }),
 });
